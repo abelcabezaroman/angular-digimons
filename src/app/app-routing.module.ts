877dae8b8a0acb44a22d250d4128e14b8191a228
Abel Cabeza Román <contacto@abelcabezaroman.com>
2020-05-26 15:09:20 +0200

    home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DigimonsComponent } from "./pages/digimons/digimons.component";
 import { ContactComponent } from "./pages/contact/contact.component";
 import { DigimonDetailComponent } from "./pages/digimons/pages/digimon-detail/digimon-detail.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 
 const routes: Routes = [
-
+  {
+    path: '', component: HomeComponent
+  },
   {
     path: 'digimons', component: DigimonsComponent
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DigimonDetailComponent } from "./pages/digimons/pages/digimon-detail/digimon-detail.component";
 import { UpperPipe } from "./shared/pipes/upper.pipe";
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { UpperPipe } from "./shared/pipes/upper.pipe";
     ButtonComponent,
     DigimonDetailComponent,
     ContactComponent,
-    UpperPipe
+    UpperPipe,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
